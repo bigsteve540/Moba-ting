b532a35325273cb9ac688a71ed837f9931ca5db8
Alex Devien (HOME) <alex.devien@gmail.com>
2020-08-14 23:40:17 +0300

    Update 2

---
+++ MOBA-Thing Server/Assets/Editor/Moba-Thing/Util Actions/RemoveMissingScripts.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class RemoveMissingScripts : Editor
+{
+    [MenuItem("Moba-Thing/Util Editor Actions/Remove Missing References in GameObjects")]
+    public static void Remove()
+    {
+        var objs = Resources.FindObjectsOfTypeAll<GameObject>();
+        int count = objs.Sum(GameObjectUtility.RemoveMonoBehavioursWithMissingScript);
+        Debug.Log($"Removed {count} missing scripts from gameobjects in current scene.");
+    }
+}
